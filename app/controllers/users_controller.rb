00fca605818bb11f9122410aecd91002a3dbd67e
suzuki77 <epitafu77@gmail.com>
2017-02-11 11:41:17 +0000

    add followings/followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,17 @@ class UsersController < ApplicationController
     end
   end
   
+  def followings
+    @user  = User.find(params[:id])
+    @users = @user.following_users
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.follower_users
+  end
+    
+  
   private
   
   def user_params
