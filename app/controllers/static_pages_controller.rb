a703acb2777abaa3edf9c9484d07ea55b79a718d
suzuki77 <epitafu77@gmail.com>
2017-02-12 14:24:39 +0000

    heroku02

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+   if logged_in?
     @micropost = current_user.microposts.build
     @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page]).per(5)
+   end
   end
   
-  # def page
-  #   @microposts =  @micropost.page(params[:page]).per(5)
-  # end
 end
